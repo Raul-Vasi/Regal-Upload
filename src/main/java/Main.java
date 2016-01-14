259c0da2307072878cdc02f6c2b9ca76a2d3d1ab
Raul Vasi <vasi@hbz-nrw.de>
2016-01-14 10:59:28 +0100

    Add Readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /*Copyright (c) 2015 "hbz"
 
 This file is part of ebooky.
@@ -20,22 +23,24 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
  * @author Raul Vasi
  *
  */
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
+
+	final static Logger logger = LoggerFactory.getLogger(Uploader.class);
+
 	public static void main(String[] args) {
-		// webclient.createResource(t, dtlBean);
 
-		/// Ein Objekt im Repository anlegen mit apache HttpClient oder jersey
-		/// Client
-		// PUT /resource/:pid -d{"contentType": dtlBean.getType,
-		/// "accessScheme":"....","publishScheme":"....","parentPid":""}
-		// PUT /resource/:pid/metadata
+		if (args.length != 0) {
+			Uploader upload = new Uploader(args[0]);
+		} else {
+			logger.warn("No input Directory was found!\n" + "!You must introduce a valid Directory!");
+			System.exit(0);
+		}
 
-		@SuppressWarnings("unused")
-		Uploader upload = new Uploader("");
 	}
 
 }
